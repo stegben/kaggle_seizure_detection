ec4334e71065ae300d34d0e85ab5c4a541d7af43
cph <cph@titan.r444b.ee.ntu.edu.tw>
2016-12-02 19:45:44 +0800

    generate image faster

--- image_generation.py
+++ image_generation.py
@@ -1,7 +1,9 @@
+import os
 import sys
 import glob
 
 import numpy as np
+import pandas as pd
 import scipy.io as sio
 
 from sklearn.cross_validation import LabelShuffleSplit
@@ -18,23 +20,37 @@ from utils import bin_power
 
 CACHE_DIR = '../nn_cache'
 RAW_DATA_DIR = '../raw_data/'
+SAFE_DATA_LABEL = '../raw_data/train_and_test_data_labels_safe.csv'
 FRAME_SIZE = 400 * 10
 FRAME_SPACING = 400 * 5
+USE_POWER = False
 
 memory = Memory(cachedir=CACHE_DIR, verbose=1)
 
 
 @memory.cache
-def get_all_file_name(path_pattern):
+def get_all_file_name(path_pattern, is_train):
+    df_correction = pd.read_csv(SAFE_DATA_LABEL)
+    safe_train = set(df_correction[df_correction['safe'] == 1]['image'].tolist())
     all_fpath = []
     for fpath in glob.glob(path_pattern):
+        if is_train:
+            if os.path.basename(fpath) not in safe_train:
+                print('drop: {} because unsafe'.format(fpath))
+                continue
+            if os.path.getsize(fpath) < 60001:
+                print('drop: {} due to filesize'.format(fpath))
+                continue
         all_fpath.append(fpath)
     return len(all_fpath), all_fpath
 
 
 def gen_metadata(fpath):
     file_name = fpath.split('/')[-1]
+    if 'new' in file_name:
+        file_name = file_name[4:]
     patient_id = int(file_name.split('_')[0])
+
     try:
         label = int(file_name.split('.')[0].split('_')[2])
         hour = -1 if label == 0 else (int(file_name.split('_')[1]) - 1) // 6
@@ -60,11 +76,13 @@ def process_single_mat(processed_data, frame_idx):
 '''
 
 @memory.cache
-def create_data(path_pattern, frame_size, spacing, is_test):
-    n_samples, all_fpath = get_all_file_name(path_pattern)
+def create_data(path_pattern, frame_size, spacing, is_test, use_power=True):
+    n_samples, all_fpath = get_all_file_name(path_pattern, (not is_test))
     n_frames_per_sample = int((240000 - frame_size)/spacing + 1)
     n_channels = 16
-    freq_bin = [freq for freq in range(150)]
+    # freq_bin = [freq for freq in range(150)]
+    freq_bin = [freq for freq in range(0, 30, 1)]
+    freq_bin = freq_bin + [30, 35, 40, 50, 60, 80, 100, 120, 160, 199]
     n_freq_bins = len(freq_bin) - 1
     shape = [n_samples, n_channels, n_freq_bins, n_frames_per_sample]
     data = np.zeros(shape, dtype='float32')
@@ -84,9 +102,14 @@ def create_data(path_pattern, frame_size, spacing, is_test):
             raw_data_frame = raw_data[start:end, :]
             for channel_idx in range(n_channels):
                 signal = raw_data_frame[:, channel_idx]
-                _, power_ratio = bin_power(signal, Band=freq_bin, Fs=400)
-                power_ratio = np.nan_to_num(power_ratio)
-                data[idx, channel_idx, :, frame_idx] = power_ratio
+                if use_power:
+                    power, power_ratio = bin_power(signal, Band=freq_bin, Fs=400)
+                    power = np.nan_to_num(power)
+                    data[idx, channel_idx, :, frame_idx] = power
+                else:
+                    power, power_ratio = bin_power(signal, Band=freq_bin, Fs=400)
+                    power_ratio = np.nan_to_num(power_ratio)
+                    data[idx, channel_idx, :, frame_idx] = power_ratio
         '''
         processed_data = np.zeros((n_channels, n_freq_bins, n_frames_per_sample), dtype='float32')
         Parallel(n_jobs=4, verbose=1)(
@@ -114,7 +137,7 @@ def create_data(path_pattern, frame_size, spacing, is_test):
 
 def split_train(train_x, train_y, train_metadata):
     test_size=0.2
-    random_state=567890
+    random_state=5438
 
     train_x_true = train_x[train_y == 1]
     train_x_false = train_x[train_y != 1]
@@ -134,6 +157,7 @@ def split_train(train_x, train_y, train_metadata):
         true_idx['subtrain'] = tr_idx
         true_idx['validation'] = te_idx
 
+    # ss = ShuffleSplit(len(hours_false), n_iter=1, test_size=None, train_size=int(len(true_idx['subtrain'])*9.5), random_state=random_state)
     ss = ShuffleSplit(len(hours_false), n_iter=1, test_size=test_size, random_state=random_state)
     false_idx = {}
     for tr_idx, te_idx in ss:
@@ -144,7 +168,6 @@ def split_train(train_x, train_y, train_metadata):
     subtrain_y = np.concatenate((train_y_true[true_idx['subtrain']], train_y_false[false_idx['subtrain']]))
     validation_x = np.concatenate((train_x_true[true_idx['validation']], train_x_false[false_idx['validation']]))
     validation_y = np.concatenate((train_y_true[true_idx['validation']], train_y_false[false_idx['validation']]))
-    import ipdb; ipdb.set_trace()
     return subtrain_x, subtrain_y, validation_x, validation_y
 
 
@@ -152,16 +175,16 @@ def main():
     train_1_data_path_pattern = '../raw_data/train_1/*.mat'
     train_2_data_path_pattern = '../raw_data/train_2/*.mat'
     train_3_data_path_pattern = '../raw_data/train_3/*.mat'
-    test_1_data_path_pattern = '../raw_data/test_1/*.mat'
-    test_2_data_path_pattern = '../raw_data/test_2/*.mat'
-    test_3_data_path_pattern = '../raw_data/test_3/*.mat'
+    test_1_data_path_pattern = '../raw_data/test_1_new/*.mat'
+    test_2_data_path_pattern = '../raw_data/test_2_new/*.mat'
+    test_3_data_path_pattern = '../raw_data/test_3_new/*.mat'
 
     output_data_name = '../cnn_' + sys.argv[1]
 
     print('create_data')
-    train_1_x, train_1_y, train_1_metadata = create_data(train_1_data_path_pattern, FRAME_SIZE, FRAME_SPACING, False)
-    train_2_x, train_2_y, train_2_metadata = create_data(train_2_data_path_pattern, FRAME_SIZE, FRAME_SPACING, False)
-    train_3_x, train_3_y, train_3_metadata = create_data(train_3_data_path_pattern, FRAME_SIZE, FRAME_SPACING, False)
+    train_1_x, train_1_y, train_1_metadata = create_data(train_1_data_path_pattern, FRAME_SIZE, FRAME_SPACING, False, USE_POWER)
+    train_2_x, train_2_y, train_2_metadata = create_data(train_2_data_path_pattern, FRAME_SIZE, FRAME_SPACING, False, USE_POWER)
+    train_3_x, train_3_y, train_3_metadata = create_data(train_3_data_path_pattern, FRAME_SIZE, FRAME_SPACING, False, USE_POWER)
     test_1_x, _, test_1_metadata = create_data(test_1_data_path_pattern, FRAME_SIZE, FRAME_SPACING, True)
     test_2_x, _, test_2_metadata = create_data(test_2_data_path_pattern, FRAME_SIZE, FRAME_SPACING, True)
     test_3_x, _, test_3_metadata = create_data(test_3_data_path_pattern, FRAME_SIZE, FRAME_SPACING, True)
@@ -169,7 +192,7 @@ def main():
     subtrain_x_1, subtrain_y_1, validation_x_1, validation_y_1 = split_train(train_1_x, train_1_y, train_1_metadata)
     subtrain_x_2, subtrain_y_2, validation_x_2, validation_y_2 = split_train(train_2_x, train_2_y, train_2_metadata)
     subtrain_x_3, subtrain_y_3, validation_x_3, validation_y_3 = split_train(train_3_x, train_3_y, train_3_metadata)
-    import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
     data = {
         'pid_1': {
             'subtrain_x': subtrain_x_1,
